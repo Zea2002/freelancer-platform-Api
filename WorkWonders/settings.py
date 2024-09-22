d03df7b8bdaa7435fee91abf8c7c92d8267e8347
Zeaur Rahman <rahmanzea31@gmail.com>
2024-09-22 23:03:02 +0600

    update

--- WorkWonders/settings.py
+++ WorkWonders/settings.py
@@ -9,7 +9,7 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure--lx3*umpx0jxawt&f2tr5gv0nevx^hmerkwkom+ui#dkue$me@'
 
 DEBUG = True
 

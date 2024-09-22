ad4c04153cbb97c091cc748c26e8e5f1f28de863
Zeaur Rahman <rahmanzea31@gmail.com>
2024-09-22 22:57:04 +0600

    update

--- WorkWonders/settings.py
+++ WorkWonders/settings.py
@@ -9,7 +9,7 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default='your-secret-key')
+SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = True
 

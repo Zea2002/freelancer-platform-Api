7c5262cfb916086603e16b48d5108018e94a6ae8
Zeaur Rahman <rahmanzea31@gmail.com>
2024-09-26 03:53:10 +0600

    update

--- user/serializers.py
+++ user/serializers.py
@@ -63,7 +63,7 @@ class LoginSerializer(serializers.Serializer):
         password = data.get('password')
 
         if email and password:
-            user = authenticate(username=email, password=password)
+            user = authenticate(email=email, password=password)
 
             if user is None:
                 raise serializers.ValidationError(_('Invalid email or password'))

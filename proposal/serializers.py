f5e498db16b1e0ff1e5317ace386b8f832d02661
Zeaur Rahman <rahmanzea31@gmail.com>
2024-09-29 01:08:18 +0600

    update

--- proposal/serializers.py
+++ proposal/serializers.py
@@ -4,7 +4,7 @@ from user.models import FreelancerProfile
 from project.models import Project
 
 class ProposalSerializer(serializers.ModelSerializer):
-    freelancer = serializers.SlugRelatedField(slug_field='user.username', queryset=FreelancerProfile.objects.all())
+    freelancer = serializers.SlugRelatedField(slug_field='user__username', queryset=FreelancerProfile.objects.all())
     project= serializers.SlugRelatedField(slug_field='title', queryset=Project.objects.all())
     class Meta:
         model = Proposal

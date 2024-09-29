2936c354b1e37bb8180c41057cb0b64c2efa7ae7
Zeaur Rahman <rahmanzea31@gmail.com>
2024-09-29 18:43:02 +0600

    update

--- proposal/serializers.py
+++ proposal/serializers.py
@@ -4,8 +4,14 @@ from user.models import FreelancerProfile
 from project.models import Project
 
 class ProposalSerializer(serializers.ModelSerializer):
-    freelancer = serializers.SlugRelatedField(slug_field='user__username', queryset=FreelancerProfile.objects.all())
-    project= serializers.SlugRelatedField(slug_field='title', queryset=Project.objects.all())
+    freelancer = serializers.PrimaryKeyRelatedField(queryset=FreelancerProfile.objects.all())
+    project = serializers.SlugRelatedField(slug_field='title', queryset=Project.objects.all())
+
     class Meta:
         model = Proposal
         fields = '__all__'
+
+    def validate_freelancer(self, value):
+        if not isinstance(value, FreelancerProfile):
+            raise serializers.ValidationError("The freelancer must be a valid freelancer profile.")
+        return value

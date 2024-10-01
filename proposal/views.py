e9d0192cec6d60d70d68fa373baea8fd328d1219
Zeaur Rahman <rahmanzea31@gmail.com>
2024-10-01 18:45:00 +0600

    update

--- proposal/views.py
+++ proposal/views.py
@@ -2,31 +2,25 @@ from rest_framework import viewsets, permissions
 from .models import Proposal
 from user.models import User
 from .serializers import ProposalSerializer
-from user.permission import IsFreelancer
 from .pagination import ProposalPagination
-from rest_framework import serializers
+
 
 class ProposalViewSet(viewsets.ModelViewSet):
     queryset = Proposal.objects.all()
     serializer_class = ProposalSerializer
     pagination_class = ProposalPagination
-    # permission_classes=[IsFreelancer]
+    permission_classes = [permissions.IsAuthenticated] 
 
-    def perform_create(self, serializer):
-        # Check if user is freelancer and has a freelancer profile
-        if self.request.user.user_type == User.FREELANCER and hasattr(self.request.user, 'freelancer_profile'):
-            freelancer_profile = self.request.user.freelancer_profile
-            serializer.save(freelancer=freelancer_profile)
-        else:
-            raise serializers.ValidationError("User is not a freelancer.")
     
     def get_queryset(self):
         queryset = Proposal.objects.all()
 
+        
         freelancer_id = self.request.query_params.get('freelancer', None)
         project_id = self.request.query_params.get('project', None)
         status = self.request.query_params.get('status', None)
 
+     
         if freelancer_id:
             queryset = queryset.filter(freelancer_id=freelancer_id)
         if project_id:

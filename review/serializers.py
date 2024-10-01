9f15c94148951f364cd9fcf1f07180385cb58a61
Zeaur Rahman <rahmanzea31@gmail.com>
2024-10-01 19:17:27 +0600

    update

--- review/serializers.py
+++ review/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .import models
-from user.models import FreelancerProfile,ClientProfile
+from user.models import FreelancerProfile, ClientProfile
 from project.models import Project
 
 class ReviewSerializer(serializers.ModelSerializer):
-    freelancer = serializers.SlugRelatedField(slug_field='user.username', queryset=FreelancerProfile.objects.all())
+    freelancer = serializers.SlugRelatedField(slug_field='user__username', queryset=FreelancerProfile.objects.all())
     project = serializers.SlugRelatedField(slug_field='title', queryset=Project.objects.all())
-    reviewer = serializers.SlugRelatedField(slug_field='user.username', queryset=ClientProfile.objects.all())
+    reviewer = serializers.SlugRelatedField(slug_field='user__username', queryset=ClientProfile.objects.all())
+    
     class Meta:
         model = models.Review
         fields = '__all__'
--- review/views.py
+++ review/views.py
@@ -11,32 +11,10 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     # permission_classes = [IsClientOrReadOnly]
 
-    def get_permissions(self):
-        if self.action == 'create':
-            self.permission_classes = [IsClient]
-        return super().get_permissions()
-
     def get_queryset(self):
         project_id = self.request.query_params.get('project')
         if project_id:
             return Review.objects.filter(project__id=project_id)
         return Review.objects.all()
 
-    def perform_create(self, serializer):
-        project_id = self.request.data.get('project')
-        freelancer_id = self.request.data.get('freelancer')
-
-        project = Project.objects.get(id=project_id)
-
-        if project.client != self.request.user.client_profile:
-            raise ValidationError("You can only review projects you have commissioned.")
-
-        try:
-            proposal = Proposal.objects.get(project=project, freelancer__id=freelancer_id)
-        except Proposal.DoesNotExist:
-            raise ValidationError("No matching proposal found for this freelancer and project.")
-
-        if proposal.proposal_status != 'Completed':
-            raise ValidationError("You cannot review a project that is not completed.")
-
-        serializer.save(reviewer=self.request.user.client_profile, project=project, freelancer=proposal.freelancer)
+   

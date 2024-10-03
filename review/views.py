5112c7a23784450b0a16a44f31e2d895dcc2c566
Zeaur Rahman <rahmanzea31@gmail.com>
2024-10-03 22:30:26 +0600

    update

--- review/views.py
+++ review/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets, permissions
 from rest_framework.exceptions import ValidationError
 from .models import Review
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ReviewSerializer
+from rest_framework.filters import OrderingFilter
 from project.models import Project
 from proposal.models import Proposal
 from user.permission import IsClientOrReadOnly,IsClient
@@ -10,6 +12,8 @@ class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     # permission_classes = [IsClientOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['freelancer'] 
 
     def get_queryset(self):
         project_id = self.request.query_params.get('project')
